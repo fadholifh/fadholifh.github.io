d08989322a64d337f02eb2fc4946c3a3942d8679
fadholifh <fadholifh14@gmail.com>
2019-12-14 20:30:21 +0700

    menambah font awesome dan fitur social

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,144 +1,167 @@
 var inputReady = true;
-var input = $('.404-input');
+var input = $(".404-input");
 input.focus();
-$('.container').on('click', function(e){
-  input.focus();
+$(".container").on("click", function (e) {
+	input.focus();
 });
 
-input.on('keyup', function(e){
-  $('.new-output').text(input.val());
-  // console.log(inputReady);
+input.on("keyup", function (e) {
+	$(".new-output").text(input.val());
+	// console.log(inputReady);
 });
 
-$('.four-oh-four-form').on('submit', function(e){
-  e.preventDefault();
-  var val = $(this).children($('.404-input')).val().toLowerCase();
-  var href;
+$(".four-oh-four-form").on("submit", function (e) {
+	e.preventDefault();
+	var val = $(this)
+		.children($(".404-input"))
+		.val()
+		.toLowerCase();
+	var href;
 
-	 if (val === 'fad'){
-    showKittens();
-  }else {
-    resetForm();
-  }
+	if (val === "fad") {
+		showKittens();
+	} else if (val === "help") {
+		help();
+	} else if (val === "social") {
+		social();
+	} else {
+		resetForm();
+	}
 });
 
-function resetForm(withKittens){
-  var message = "Sorry that command is not recognized."
-  var input = $('.404-input');
+function resetForm(withKittens) {
+	var message = "Sorry that command is not recognized.";
+	var input = $(".404-input");
 
-  if (withKittens){
-    $('.kittens').removeClass('kittens');
-    message = "Huzzzzzah!"
-  }
+	if (withKittens) {
+		$(".kittens").removeClass("kittens");
+		message = "Done!";
+	}
 
-  $('.new-output').removeClass('new-output');
-  input.val('');
-  $('.terminal').append('<p class="prompt">' + message + '</p><p class="prompt output new-output"></p>');
+	$(".new-output").removeClass("new-output");
+	input.val("");
+	$(".terminal").append(
+		'<p class="prompt">' +
+		message +
+		'</p><p class="prompt output new-output"></p>'
+	);
 
-  $('.new-output').velocity(
-    'scroll'
-  ), {duration: 100}
+	$(".new-output").velocity("scroll"), {
+		duration: 100
+	};
 }
 
-	function showKittens(){
-		$('.terminal').append("<div class='kittens'>"+
-								 "<p class='prompt'>	                             ,----,         ,----,                                          ,---,</p>" +
-								 "<p class='prompt'>       ,--.                ,/   .`|       ,/   .`|                     ,--.              ,`--.' |</p>" +
-								 "<p class='prompt'>   ,--/  /|    ,---,     ,`   .'  :     ,`   .'  :     ,---,.        ,--.'|   .--.--.    |   :  :</p>" +
-								 "<p class='prompt'>,---,': / ' ,`--.' |   ;    ;     /   ;    ;     /   ,'  .' |    ,--,:  : |  /  /    '.  '   '  ;</p>" +
-								 "<p class='prompt'>:   : '/ /  |   :  : .'___,/    ,'  .'___,/    ,'  ,---.'   | ,`--.'`|  ' : |  :  /`. /  |   |  |</p>" +
-								 "<p class='prompt'>|   '   ,   :   |  ' |    :     |   |    :     |   |   |   .' |   :  :  | | ;  |  |--`   '   :  ;</p>" +
-								 "<p class='prompt'>'   |  /    |   :  | ;    |.';  ;   ;    |.';  ;   :   :  |-, :   |   \\ | : |  :  ;_     |   |  '</p>" +
-								 "<p class='prompt'>|   ;  ;    '   '  ; `----'  |  |   `----'  |  |   :   |  ;/| |   : '  '; |  \\  \\    `.  '   :  |</p>" +
-								 "<p class='prompt'>:   '   \\   |   |  |     '   :  ;       '   :  ;   |   :   .' '   ' ;.    ;   `----.   \\ ;   |  ;</p>" +
-								 "<p class='prompt'>'   : |.  \\ |   |  '     '   :  |       '   :  |   '   :  ;/| '   : |  ; .'  /  /`--'  /  `--..`;  </p>" +
-								 "<p class='prompt'>|   | '_\\.' '   :  |     ;   |.'        ;   |.'    |   |    \\ |   | '`--'   '--'.     /  .--,_   </p>" +
-								 "<p class='prompt'>'   : |     ;   |.'      '---'          '---'      |   :   .' '   : |         `--'---'   |    |`.  </p>" +
-								 "<p class='prompt'>;   |,'     '---'                                  |   | ,'   ;   |.'                    `-- -`, ; </p>" +
-								 "<p class='prompt'>'---'                                              `----'     '---'                        '---`'</p>" +
-								 "<p class='prompt'>                                                              </p></div>");
+function showKittens() {
+	$(".terminal").append(
+		"<div class='kittens'>" +
+		"<p class='prompt'>	                             ,----,         ,----,                                          ,---,</p>" +
+		"<p class='prompt'>       ,--.                ,/   .`|       ,/   .`|                     ,--.              ,`--.' |</p>" +
+		"<p class='prompt'>   ,--/  /|    ,---,     ,`   .'  :     ,`   .'  :     ,---,.        ,--.'|   .--.--.    |   :  :</p>" +
+		"<p class='prompt'>,---,': / ' ,`--.' |   ;    ;     /   ;    ;     /   ,'  .' |    ,--,:  : |  /  /    '.  '   '  ;</p>" +
+		"<p class='prompt'>:   : '/ /  |   :  : .'___,/    ,'  .'___,/    ,'  ,---.'   | ,`--.'`|  ' : |  :  /`. /  |   |  |</p>" +
+		"<p class='prompt'>|   '   ,   :   |  ' |    :     |   |    :     |   |   |   .' |   :  :  | | ;  |  |--`   '   :  ;</p>" +
+		"<p class='prompt'>'   |  /    |   :  | ;    |.';  ;   ;    |.';  ;   :   :  |-, :   |   \\ | : |  :  ;_     |   |  '</p>" +
+		"<p class='prompt'>|   ;  ;    '   '  ; `----'  |  |   `----'  |  |   :   |  ;/| |   : '  '; |  \\  \\    `.  '   :  |</p>" +
+		"<p class='prompt'>:   '   \\   |   |  |     '   :  ;       '   :  ;   |   :   .' '   ' ;.    ;   `----.   \\ ;   |  ;</p>" +
+		"<p class='prompt'>'   : |.  \\ |   |  '     '   :  |       '   :  |   '   :  ;/| '   : |  ; .'  /  /`--'  /  `--..`;  </p>" +
+		"<p class='prompt'>|   | '_\\.' '   :  |     ;   |.'        ;   |.'    |   |    \\ |   | '`--'   '--'.     /  .--,_   </p>" +
+		"<p class='prompt'>'   : |     ;   |.'      '---'          '---'      |   :   .' '   : |         `--'---'   |    |`.  </p>" +
+		"<p class='prompt'>;   |,'     '---'                                  |   | ,'   ;   |.'                    `-- -`, ; </p>" +
+		"<p class='prompt'>'---'                                              `----'     '---'                        '---`'</p>" +
+		"<p class='prompt'>                                                              </p></div>"
+	);
 
-		
-		var lines = $('.kittens p');
-		$.each(lines, function(index, line){
-			setTimeout(function(){
-				$(line).css({
-					"opacity": 1
-				});
+	var lines = $(".kittens p");
+	$.each(lines, function (index, line) {
+		setTimeout(function () {
+			$(line).css({
+				opacity: 1
+			});
 
-				textEffect($(line))
-			}, index * 100);
-		});
+			textEffect($(line));
+		}, index * 100);
+	});
 
-		$('.new-output').velocity(
-			'scroll',
-			
-		), {duration: 100}
-
-		setTimeout(function(){
-			resetForm(true)
-		}, (lines.length * 100) + 1000);
-	}
-
-	function textEffect(line){
-		var alpha = [';', '.', ',', ':', ';', '~', '`'];
-		var animationSpeed = 10;
-		var index = 0;
-		var string = line.text();
-		var splitString = string.split("");
-		var copyString = splitString.slice(0);
-
-		var emptyString = copyString.map(function(el){
-		    return [alpha[Math.floor(Math.random() * (alpha.length))], index++];
-		})
-
-		emptyString = shuffle(emptyString);
-
-		$.each(copyString, function(i, el){
-		    var newChar = emptyString[i];
-		    toUnderscore(copyString, line, newChar);
-
-		    setTimeout(function(){
-		      fromUnderscore(copyString, splitString, newChar, line);
-		    },i * animationSpeed);
-		  })
-	}
-
-	function toUnderscore(copyString, line, newChar){
-		copyString[newChar[1]] = newChar[0];
-		line.text(copyString.join(''));
-	}
-
-	function fromUnderscore(copyString, splitString, newChar, line){
-		copyString[newChar[1]] = splitString[newChar[1]];
-		line.text(copyString.join(""));
-	}
-
-
-	function shuffle(o){
-	    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	    return o;
+	$(".new-output").velocity("scroll"), {
+		duration: 100
 	};
 
+	setTimeout(function () {
+		resetForm(true);
+	}, lines.length * 100 + 1000);
+}
 
+function social() {
+	$(".terminal").append(
+		`<p class='prompt'>Social
+			<span>
+  <a href='mailto:fadholifh14@gmail.com' target='_top' class='soc'><i class='fa fa-envelope s72'></i></a> <a href='https://github.com/fadholifh' target='_blank' class='soc'><i class='fab fa-github s72'></i></a> <a href='https://linkedin.com/in/fadholi-fat-haranto-30765b14b' target='_blank' class='soc'><i class='fab fa-linkedin s72'></i></a>
+			</span>
+  		</p>`
+	);
 
-window.setTimeout("waktu()", 1000);
-n =  new Date();
+	var lines = $(".kittens p");
+	$.each(lines, function (index, line) {
+		setTimeout(function () {
+			$(line).css({
+				opacity: 1
+			});
+
+			textEffect($(line));
+		}, index * 100);
+	});
+
+	$(".new-output").velocity("scroll"), {
+		duration: 100
+	};
+
+	setTimeout(function () {
+		resetForm(true);
+	}, lines.length * 100 + 0);
+}
+
+function help() {
+	var message = `<p class='prompt'>You can use command below
+  fad
+  about
+  social </p>`;
+	$(".terminal").append(`${message}`);
+	var lines = $(".kittens p");
+	$.each(lines, function (index, line) {
+		setTimeout(function () {
+			$(line).css({
+				opacity: 1
+			});
+
+			textEffect($(line));
+		}, index * 100);
+	});
+
+	$(".new-output").velocity("scroll"), {
+		duration: 0
+	};
+
+	setTimeout(function () {
+		resetForm(true);
+	}, lines.length * 100 + 1);
+}
+
+n = new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 
 document.getElementById("date").innerHTML = `${m}/${d}/${y}|`;
 
-$(document).ready(function(){
+$(document).ready(function () {
 	function clock() {
-	  let now = new Date();
-	  let secs = ('0' + now.getSeconds()).slice(-2);
-	  let mins = ('0' + now.getMinutes()).slice(-2);
-	  let hr = now.getHours();
-	  let Time = hr + ":" + mins + ":" + secs;
-	  document.getElementById("time").innerHTML = Time;
-	  requestAnimationFrame(clock);
+		let now = new Date();
+		let secs = ("0" + now.getSeconds()).slice(-2);
+		let mins = ("0" + now.getMinutes()).slice(-2);
+		let hr = now.getHours();
+		let Time = hr + ":" + mins + ":" + secs;
+		document.getElementById("time").innerHTML = Time;
+		requestAnimationFrame(clock);
 	}
 
 	requestAnimationFrame(clock);
